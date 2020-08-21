2c187b484a9f19cb9c307964d77cb532aba0804f
Eldar Dzhunushaliev <eldar.junushaliev@tucana.aero>
2020-08-21 12:50:06 +0300

    Input check

--- js/script.js
+++ js/script.js
@@ -1,26 +1,42 @@
 "use strict";
 
 const numberOfFilms = +prompt('How many films have you watched?', '');
+if (numberOfFilms < 10) {
+    alert("You've seen few films");
+} else if (numberOfFilms < 30) {
+    alert("You are an average customer");
+} else {
+    alert("Oh, you're an expirienced one");
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    private: false
 };
 
-let lastMovie = prompt('Call one of the last films you seen', ''),
-    lastScore = prompt('How do you rate it?', '');
+let lastMovie = "",
+    lastScore = 0,
+    correctInput = false;
 
-personalMovieDB.movies[lastMovie] = lastScore;
-
-lastMovie = prompt('Call one of the last films you seen', '');
-lastScore = prompt('How do you rate it?', '');
-
-personalMovieDB.movies[lastMovie] = lastScore;
+for (let i = 1; i<=2; i++) {
+    do {
+        lastMovie = prompt('Call one of the last films you seen', '');
+        correctInput = (lastMovie != null) && (lastMovie != '') && (lastMovie.length < 50);
+        if (!correctInput) {
+            alert('Incorrect input. Repeat, please.');
+        }
+    } while (!correctInput);
+    do {
+        lastScore = +prompt('How do you rate it?', '');
+        correctInput = (lastScore != null);
+        if (!correctInput) {
+            alert('Incorrect input. Repeat, please.');
+        }
+    } while (!correctInput);
+    personalMovieDB.movies[lastMovie] = lastScore;
+}
 
 console.log(personalMovieDB);
-
-//test comment#2
-// test comment#3
