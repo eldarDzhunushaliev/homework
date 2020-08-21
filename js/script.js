1b891e44c7bf785398c19ea8a9b0454cb32b96dc
Eldar Dzhunushaliev <eldar.junushaliev@tucana.aero>
2020-08-21 17:46:51 +0300

    Functions added

--- js/script.js
+++ js/script.js
@@ -1,54 +1,86 @@
 "use strict";
 
-let numberOfFilms = 0,
-    lastMovie = "",
-    lastScore = 0,
-    correctInput = false;
+let numberOfFilms,
+    lastMovie,
+    lastScore,
+    inputGenre,
+    correctInput;
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     private: false
-};
+};    
 
-do {
-    numberOfFilms = prompt('How many films have you watched?', '');
-    correctInput = !isNaN(+numberOfFilms) && numberOfFilms != null && numberOfFilms != '';
-    if (!correctInput) {
-        alert('Incorrect input. Repeat, please.');
+function start() {
+    personalMovieDB.private = confirm('Do you want your data base to be private?');
+    do {
+        numberOfFilms = prompt('How many films have you watched?', '');
+        correctInput = !isNaN(numberOfFilms) && numberOfFilms != null && numberOfFilms != '';
+        if (!correctInput) {
+            alert('Incorrect input. Repeat, please.');
+        }
+    } while (!correctInput);
+    personalMovieDB.count = +numberOfFilms;
+    rateUser();
+}
+
+start();
+
+function rateUser() {
+    if (personalMovieDB.count < 10) {
+        alert("You've seen few films");
+    } else if (personalMovieDB.count < 30) {
+        alert("You are an average customer");
     } else {
-        numberOfFilms = +numberOfFilms;
+        alert("Oh, you're an expirienced one");
     }
-} while (!correctInput);
-
-if (numberOfFilms < 10) {
-    alert("You've seen few films");
-} else if (numberOfFilms < 30) {
-    alert("You are an average customer");
-} else {
-    alert("Oh, you're an expirienced one");
 }
 
-for (let i = 0; i < 2; i++) {
-    do {
-        lastMovie = prompt('Call one of the last films you seen', '');
-        correctInput = lastMovie != null && lastMovie != '' && lastMovie.length < 50;
-        if (!correctInput) {
-            alert('Incorrect input. Repeat, please.');
-        }
-    } while (!correctInput);
-    do {
-        lastScore = prompt('How do you rate it?', '');
-        correctInput = !isNaN(+lastScore) && lastScore != null && lastScore != '';
-        if (!correctInput) {
-            alert('Incorrect input. Repeat, please.');
-        } else {
-            lastScore = +lastScore;
-        }
-    } while (!correctInput);
-    personalMovieDB.movies[lastMovie] = lastScore;
+function askRecentMovies() {
+    for (let i = 0; i < 2; i++) {
+        do {
+            lastMovie = prompt('Call one of the last movies you seen', '');
+            correctInput = lastMovie != null && lastMovie != '' && lastMovie.length < 50;
+            if (!correctInput) {
+                alert('Incorrect input. Repeat, please.');
+            }
+        } while (!correctInput);
+        do {
+            lastScore = prompt('How do you rate it?', '');
+            correctInput = !isNaN(lastScore) && lastScore != null && lastScore != '';
+            if (!correctInput) {
+                alert('Incorrect input. Repeat, please.');
+            }
+        } while (!correctInput);
+        personalMovieDB.movies[lastMovie] = +lastScore;
+    }
 }
 
-console.log(personalMovieDB);
+askRecentMovies();
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        do {
+            inputGenre = prompt(`What is your favourite genre №${i+1}?`, '');
+            correctInput = inputGenre != null && inputGenre != '' && inputGenre.length < 50;
+            if (!correctInput) {
+                alert('Incorrect input. Repeat, please.');
+            }
+        } while (!correctInput);
+
+        personalMovieDB.genres[i] = inputGenre;
+    }
+}
+
+writeYourGenres();
+
+function showMyDB(isprivate) {
+    if (!isprivate) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.private);
